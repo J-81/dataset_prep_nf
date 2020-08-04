13ecec5b198204536119dd1fd31451223027ae69
J-81 <jonathan.d.oribello@gmail.com>
2020-08-04 09:41:43 -0700

    Doubled mem, add 1 day timelimit

--- utility/nextflow_run.sh
+++ utility/nextflow_run.sh
@@ -5,7 +5,8 @@
 #SBATCH -o std_output_%j.out
 #SBATCH -e std_error_%j.err
 #SBATCH -c 1
-#SBATCH --mem=4gb
+#SBATCH --mem=8gb
+#SBATCH --time=23:59:59
 #SBATCH -D /home/joribello/nextflow_workflows
 # #SBATCH --test-only
 
@@ -13,4 +14,4 @@ export TOWER_ACCESS_TOKEN=7e38ad64660cf92608d67a9425562bc55722d54c
 export NXF_VER=20.07.1
 
 
-./nextflow run test.nf -profile slurm
+./nextflow run main.nf -profile sjsu_slurm -c config/dataset_v1.config -resume -with-tower --limiter -1 
