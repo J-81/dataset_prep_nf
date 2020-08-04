574e7622f24545c6ebdff9002bc8bc1dca941bab
J-81 <jonathan.d.oribello@gmail.com>
2020-08-04 09:49:02 -0700

    Now outputs slurm logs to a dedicted folder

--- utility/nextflow_run.sh
+++ utility/nextflow_run.sh
@@ -2,8 +2,8 @@
 
 #SBATCH -p nodes
 #SBATCH --job-name="nf_wf_manage"
-#SBATCH -o std_output_%j.out
-#SBATCH -e std_error_%j.err
+#SBATCH -o slurm_logs/std_output_%j.out
+#SBATCH -e slurm_logs/std_error_%j.err
 #SBATCH -c 1
 #SBATCH --mem=8gb
 #SBATCH --time=23:59:59
@@ -14,4 +14,4 @@ export TOWER_ACCESS_TOKEN=7e38ad64660cf92608d67a9425562bc55722d54c
 export NXF_VER=20.07.1
 
 
-./nextflow run main.nf -profile sjsu_slurm -c config/dataset_v1.config -resume -with-tower --limiter -1 
+./nextflow run main.nf -profile sjsu_slurm -c config/dataset_v1.config -resume -with-tower --limiter -1
