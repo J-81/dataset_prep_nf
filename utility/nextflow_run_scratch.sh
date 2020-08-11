10f7807d51fda658e9219cd7e3c9bb2fa8471201
J-81 <jonathan.d.oribello@gmail.com>
2020-08-11 10:34:02 -0700

    Updated to reserve 4 cpus, for very short jobs not worth new slurm job

--- utility/nextflow_run_scratch.sh
+++ utility/nextflow_run_scratch.sh
@@ -4,8 +4,8 @@
 #SBATCH --job-name="nf_wf_manage"
 #SBATCH -o slurm_logs/std_output_%j.out
 #SBATCH -e slurm_logs/std_error_%j.err
-#SBATCH -c 1
-#SBATCH --mem=8gb
+#SBATCH -c 4
+#SBATCH --mem=16gb
 #SBATCH --time=23:59:59
 #SBATCH -D /home/joribello/nextflow_workflows
 # #SBATCH --test-only
