8aa8ee32482d8f18b462d97d9e2bc337f01c2047
Jonathan Oribello <jonathan.d.oribello@gmail.com>
2020-09-22 14:00:51 -0700

    Reworked to fit better separation of workflow code and actual working directory for pipeline

--- utility/nextflow_run.sh
+++ utility/nextflow_run.sh
@@ -4,14 +4,13 @@
 #SBATCH --job-name="nf_wf_manage"
 #SBATCH -o slurm_logs/std_output_%j.out
 #SBATCH -e slurm_logs/std_error_%j.err
-#SBATCH -c 1
-#SBATCH --mem=8gb
+#SBATCH -c 4
+#SBATCH --mem=16gb
 #SBATCH --time=23:59:59
-#SBATCH -D /home/joribello/nextflow_workflows
 # #SBATCH --test-only
 
 export TOWER_ACCESS_TOKEN=7e38ad64660cf92608d67a9425562bc55722d54c
 export NXF_VER=20.07.1
 
 
-./nextflow run main.nf -profile sjsu_slurm -c config/dataset_v1.config -resume -with-tower --limiter 8
+nextflow run main.nf -profile sjsu_slurm -c config/dataset_v1.config -resume -with-tower --limiter 800 -w /home/joribello/scopDS/scopDB_v2/work
